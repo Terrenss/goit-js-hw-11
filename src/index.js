690094bc69929dde566d0169bc185bd6d35d2a61
Artur <terrenss666@gmail.com>
2023-07-17 16:53:27 +0200

    style css

--- src/index.js
+++ src/index.js
@@ -1,100 +1,168 @@
-import axios from "axios";
+import './style/style.css';
+import axios from 'axios';
 import Notiflix from 'notiflix';
-const BASE_URL = "https://pixabay.com/api/?key=38201775-25af2f6387845f79e6ba89182";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const form = document.querySelector('#search-form')
-const gallery = document.querySelector(".gallery")
-const searchMore = document.querySelector(".load-more")
-let searchQuery = '';
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+
+const infiniteScroll = document.querySelector('.js-scroll')
+
+Notiflix.Notify.init({
+    position: 'center-top',
+    distance: '45px',
+    timeout: 2000,
+    cssAnimationStyle: 'zoom',
+    fontFamily: 'Arial, sans-serif',
+});
+
+let lightbox;
+let options = {
+  root: null,
+  rootMargin: "200px",
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoadPage, options);
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '36956976-ac77797dbc5715892646935b7';
+
 let page = 1;
-let totalCards = 0;
+let totalPage = 0
+const itemsOnPage = 40;
+let currentSearch = ''
 
-form.addEventListener('submit', getSearchData)
-searchMore.addEventListener("click", getMoreData)
+const searchParams = new URLSearchParams({
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: itemsOnPage
 
+});
 
-function getMoreData (event) {
-  fetchSearchEl(searchQuery).then((data) => {
-    createMarkup(data)
-})
+async function getData(value) {
+    const response = await axios.get(`${BASE_URL}?${searchParams}&q=${value}`);
+    return response.data;
 }
-function getSearchData(event) {
-    event.preventDefault();
+
+formEl.addEventListener('submit', handlerSearch);
+
+async function handlerSearch (e) {
+    e.preventDefault();
+
+    const { searchQuery: { value } } = e.currentTarget.elements
     
-    
-    searchQuery = event.currentTarget.elements.searchQuery.value;
-    gallery.innerHTML = "";
+    if (value.trim() === '') {
+        return notFoundMassage();
+    }
+
+    currentSearch = value;
     page = 1;
-    totalCards = 0;
-    fetchSearchEl(searchQuery).then((data) => {
-        createMarkup(data) 
-    });
-};
+    galleryEl.innerHTML = '';
 
-function createMarkup(answers) {
-    totalCards += 40;
+    await renderData()
+    observer.observe(infiniteScroll)
 
-if (answers.hits.length !== 0) {
-        
-    if (totalCards <= answers.totalHits) {
-            searchMore.style.display = "block";
-        const gallarys = answers.hits.map(answer => {
-                         
-            const webformatURL = answer.webformatURL;
-            const largeImageURL = answer.largeImageURL;
-            const tags = answer.tags;
-            const likes = answer.likes;
-            const views = answer.views;
-            const comments = answer.comments;
-            const downloads = answer.downloads;
-            gallery.insertAdjacentHTML("beforeend",
-                `<div class="photo-card">
-           <img src="${webformatURL}" width="200" height="120" alt="${tags}" loading="lazy" />
-           <div class="info">
-          <p class="info-item">
-            <b>Likes:</b>
-            <b>${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views:</b>
-            <b>${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments:</b>
-            <b>${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads:</b>
-            <b>${downloads}</b>
-          </p>
-          </div>
-         </div>`);
-            
-         });
-       
-       } else {
-        
-          searchMore.style.display = "none";
-          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-            }
-      
-} else {
-    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    }
-
+    setLightbox();
     
-    
- }
-async function fetchSearchEl(searchEl) {
+
+}
+
+async function renderData() {
     try {
-        const response = await axios(`${BASE_URL}&q=${searchEl}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
-                  page += 1;
-          
-    
-        
-      return response.data;
+        searchParams.set('page', page);
+        const data = await getData(currentSearch);
+        const { hits, totalHits } = data;
+        totalPage = Math.ceil(totalHits / itemsOnPage)
+           
+        if (hits.length === 0) {
+            return notFoundMassage();
+        }
+        if (page === 1) {
+            Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+        }
+
+        galleryEl.insertAdjacentHTML('beforeend', getMarkupItem(hits));
+    } catch (err) {
+        console.error(err.message) 
     }
-  catch {
-        
-    }
+}
+
+function getMarkupItem(arr) {
+    const photoCard = arr.map(({ tags,largeImageURL, webformatURL, views, downloads, likes, comments }) => `
+            
+            <div class="photo-card">
+                <a href="${largeImageURL}">
+                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                </a>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                        ${likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                        ${views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                        ${comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                        ${downloads}
+                    </p>
+                </div>
+                </div>`
+    ).join('')
+    return photoCard; 
 };
+
+function notFoundMassage() {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
+};
+
+function maxFoundMassage() {
+    Notiflix.Notify.warning('We&#96re sorry, but you&#96ve reached the end of search results.'); 
+}
+
+function smoothScroll() {
+ const { height: cardHeight } = galleryEl
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+}
+
+function setLightbox() {
+    lightbox = new SimpleLightbox('.photo-card a', {
+    navText: ['&#10094;', '&#10095;'],
+    captionsData: 'alt',
+    captionDelay: 250,
+    showCounter: false,
+   });
+}
+
+async function onLoadPage(entries, observer) {
+    entries.forEach(async (entry) => {
+        
+        if (entry.isIntersecting) {
+            page += 1;
+            
+            if (page > totalPage) {
+                maxFoundMassage();
+                observer.unobserve(infiniteScroll);
+                return
+            }
+            await renderData();
+            smoothScroll();
+            lightbox.refresh();
+        }
+    });
+}
